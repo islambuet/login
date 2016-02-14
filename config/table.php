84ee5d5826a105fef8226a6832139d5a7a381ad4
islambuet <islambuet@gmail.com>
2016-02-14 08:39:10 +0600

    1st application

---
+++ config/table.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['table_users'] = 'system_user';
+$config['table_user_group'] = 'system_group';
+$config['table_task'] = 'system_task';
+$config['table_user_group_role'] = 'system_user_group_role';
+$config['table_history'] = 'system_history';
+
---
+++ language/english/login_lang.php
@@ -0,0 +1,177 @@
+<?php
+
+//login
+$lang['MSG_USERNAME_PASSWORD_INVALID'] = 'Username and Password did not match or Invalid';
+$lang['MSG_LOGIN_SUCCESS'] = 'Successfully Logged In';
+$lang['MSG_LOGOUT_SUCCESS'] = 'Successfully Logged out';
+
+//Action buttons
+$lang['ACTION_DASHBOARD'] = 'Dashboard';
+$lang['ACTION_NEW'] = 'New';
+$lang['ACTION_EDIT'] = 'Edit';
+$lang['ACTION_LOGOUT'] = 'Logout';
+$lang['ACTION_BACK'] = 'Back';
+$lang['ACTION_SAVE'] = 'Save';
+
+
+//****MODULE TASK//
+
+$lang['LABEL_PARENT'] = 'Parent';
+$lang['LABEL_CONTROLLER_NAME'] = 'Controller';
+
+$lang['TOTAL_TASK'] = 'Number of Tasks';
+//*****//
+
+//common
+$lang['YOU_DONT_HAVE_ACCESS'] = 'You cannot access this page';
+$lang['ACTIVE'] = 'Active';
+$lang['INACTIVE'] = 'In-active';
+$lang['DELETE'] = 'Delete';
+$lang['SELECT'] = 'Select';
+
+$lang['SERIAL'] = 'Serial';
+$lang['STATUS'] = 'Status';
+$lang['ACTION'] = 'Action';
+$lang['NO_DATA_FOUND']='No Data Found';
+$lang['MSG_SAVED_SUCCESS'] = 'Successfully Saved';
+$lang['MSG_SAVED_FAIL'] = 'Failed to Save!';
+$lang['MSG_INVALID_INPUT'] = 'Invalid data entry';
+
+$lang['LABEL_DATE']='Date';
+$lang['MSG_FILE_SIZE_ERROR']='File Size Error';
+$lang['MSG_FILE_TYPE_ERROR']='File Type Error';
+
+$lang['LABEL_YES'] = 'Yes';
+$lang['LABEL_NO'] = 'No';
+
+
+$lang['LABEL_YEAR'] = 'Year';
+$lang['LABEL_NAME'] = 'Name';
+$lang['LABEL_ORDER'] = 'Ordering';
+$lang['LABEL_REMARKS'] = 'Remarks';
+$lang['SELECT_ONE_ITEM'] = 'Select a item';
+$lang['LABEL_ADD_MORE'] = 'Add more';
+$lang['LABEL_QUANTITY'] = 'Quantity';
+$lang['LABEL_TOTAL'] = 'Total';
+$lang['LABEL_SEARCH'] = 'Search';
+$lang['MSG_BOOKING_NOT_FOUND'] = 'No Booking available';
+
+//Create Crop
+$lang['LABEL_CROP_NAME'] = 'Crop Name';
+//create classification
+$lang['LABEL_CLASSIFICATION_NAME'] = 'Classification Name';
+//create type
+$lang['LABEL_TYPE_NAME'] = 'Type Name';
+//create skin
+$lang['LABEL_SKIN_TYPE_NAME'] = 'Skin Type Name';
+//create variety
+$lang['LABEL_VARIETY_NAME'] = 'Variety Name';
+$lang['LABEL_UNIT_PRICE'] = 'Price/Box';
+//Consignment
+$lang['LABEL_CONSIGNMENT_NAME'] = 'Consignment Name';
+
+//Create zone
+$lang['LABEL_ZONE_NAME'] = 'Zone';
+//Create Territory
+$lang['LABEL_TERRITORY_NAME'] = 'Territory';
+//Create District
+$lang['LABEL_DISTRICT_NAME'] = 'District';
+//Create Upazila
+$lang['LABEL_UPAZILA_NAME'] = 'Upazila';
+//Create Union
+$lang['LABEL_UNION_NAME'] = 'Union';
+//Create Customer
+$lang['LABEL_CUSTOMER_ID'] = 'Customer Id';
+$lang['LABEL_CUSTOMER_NAME'] = 'Customer Name';
+$lang['LABEL_OWNER_NAME'] = 'Owner name';
+$lang['LABEL_FATHER_NAME'] = 'Father\'s name';
+$lang['LABEL_EMAIL'] = 'E-mail';
+$lang['LABEL_CONTACT_NO'] = 'Contact No.';
+$lang['LABEL_ADDRESS'] = 'Address';
+
+//booking
+$lang['MSG_REQUIRED_BOOKING'] = 'Minimum one variety required.';
+$lang['MSG_BOOKING_QUANTITY_MISSING'] = 'Invalid Quantity';
+$lang['MSG_BOOKING_DISCOUNT_MISSING'] = 'Invalid Discount';
+$lang['MSG_BOOKING_QUANTITY_INVALID'] = 'Negative Quantity not allowed.';
+$lang['MSG_BOOKING_VARIETY_MISSING'] = 'Variety was not selected.';
+$lang['MSG_BOOKING_DUPLICATE_VARIETY'] = 'You cannot select same variety multiple times.';
+$lang['MSG_EDIT_PRIMARY'] = 'Please edit primary booking.';
+
+$lang['LABEL_PRELIMINARY_PAYMENT'] = 'Pre Advanced Payment';
+$lang['LABEL_PERMANENT_PAYMENT'] = 'Permanent Advanced Payment';
+$lang['LABEL_AMOUNT'] = 'Amount';
+$lang['LABEL_PAYMENT_METHOD'] = 'Method of payment';
+$lang['LABEL_PAYMENT_NUMBER'] = 'Number';
+$lang['LABEL_BANK_NAME'] = 'Bank Name';
+$lang['LABEL_BRANCH_NAME'] = 'Branch Name';
+$lang['MSG_BOOKING_PAYMENT_MISSING'] = 'Invalid payment amount';
+$lang['MSG_BOOKING_PAYMENT_INVALID'] = 'Negative payment amount not allowed';
+$lang['MSG_BOOKING_PAYMENT_METHOD_INVALID'] = 'Payment method was not selected';
+$lang['MSG_BOOKING_PAYMENT_NUMBER_INVALID'] = 'C/P number was empty';
+$lang['LABEL_BOOKING_ID'] = 'Booking Id';
+$lang['LABEL_BOOKING_STATUS'] = 'Booking State';
+$lang['LABEL_PRELIMINARY_BOOKING_DATE'] = 'Preliminary booking date';
+$lang['LABEL_PERMANENT_BOOKING_DATE'] = 'Permanent booking date';
+$lang['LABEL_PRELIMINARY_REMARKS'] = 'Preliminary remarks';
+$lang['LABEL_PERMANENT_REMARKS'] = 'Permanent remarks';
+$lang['LABEL_CUSTOMER_LOCATION'] = 'Customer location';
+$lang['LABEL_TOTAL_QUANTITY'] = 'Total Quantity';
+$lang['LABEL_TOTAL_PRICE'] = 'Total Amount Tk';
+$lang['LABEL_TOTAL_PRELIMINARY_PAYMENT'] = 'Pre Adv Tk.';
+$lang['LABEL_TOTAL_PERMANENT_PAYMENT'] = 'Final Adv Tk';
+$lang['LABEL_TOTAL_ADVANCED_PAYMENT'] = 'Total Adv Tk';
+$lang['LABEL_PAYMENT_FOR'] = 'Payment For';
+$lang['LABEL_MORE_PAYMENT'] = 'Add more Payment';
+
+
+
+$lang['LABEL_CONTAINER_NAME'] = 'Container Name';
+$lang['LABEL_MORE_CONTAINER'] = 'Add more Container';
+$lang['LABEL_VARIETY_QUANTITY'] = 'Variety Quantity';
+
+$lang['MSG_SET_VARIETY_PRICE'] = 'Please setup all variety price for this year';
+$lang['LABEL_ACTION'] = 'Action';
+$lang['LABEL_DISCOUNT'] = 'Discount';
+
+//allocation
+$lang['MSG_NO_PERMANENT_BOOKING_FOUND'] = 'No permanent booking found';
+$lang['MSG_NO_CONSIGNMENT_FOUND'] = 'No Consignment found';
+$lang['LABEL_EXPECTED_RECEIVE_DATE'] = 'Expected Receive Date';
+
+$lang['LABEL_MIN_QUANTITY'] = 'Minimum Quantity';
+$lang['LABEL_MAX_QUANTITY'] = 'Maximum Quantity';
+$lang['LABEL_COLOR_CODE'] = 'Color Code';
+$lang['LABEL_COLOR'] = 'Color';
+
+$lang['LABEL_PICTURE_NAME'] = 'Picture name';
+$lang['LABEL_VEHICLE_NO'] = 'Vehicle no#';
+$lang['LABEL_VEHICLE_NUMBER'] = 'Vehicle number';
+$lang['LABEL_DRIVER_NAME'] = 'Driver Name';
+$lang['LABEL_CONTACT_NO'] = 'Contact No';
+$lang['LABEL_INVOICE_NUMBER'] = 'Invoice Number';
+$lang['LABEL_BOOKING_TYPE'] = 'Booking Type';
+$lang['LABEL_VIEW_REPORT'] = 'View Report';
+
+$lang['LABEL_SELECT_COLOR'] = 'Select color';
+$lang['LABEL_BACKGROUND_COLOR'] = 'Background color';
+$lang['LABEL_BORDER_COLOR'] = 'Border color';
+
+$lang['LABEL_CONTAINER_VARIETY_TYPE'] = 'Container Type';
+$lang['LABEL_CONTAINER_NO'] = 'Container No';
+$lang['LABEL_NO_OF_VEHICLES'] = 'No of vehicles';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
