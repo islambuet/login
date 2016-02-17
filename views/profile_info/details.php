82df992898232d8ad25821aba1036ab5001f3f4a
islambuet <islambuet@gmail.com>
2016-02-18 07:34:07 +1000

    profile_info complete

--- views/profile_info/details.php
+++ views/profile_info/details.php
@@ -1,8 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $CI = & get_instance();
-$action_data=array();
-$action_data["action_back"]=base_url($CI->controller_url);
-$CI->load->view("action_buttons",$action_data);
 ?>
 
 <div class="row widget">
@@ -324,14 +321,6 @@ $CI->load->view("action_buttons",$action_data);
             </div>
         </div>
     </div>
-    <div style="" class="row show-grid">
-        <div class="col-xs-4">
-            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_ORDER');?><span style="color:#FF0000">*</span></label>
-        </div>
-        <div class="col-sm-4 col-xs-8">
-            <label class="control-label"><?php echo $user_info['ordering'];?></label>
-        </div>
-    </div>
 
 </div>
 
@@ -341,7 +330,7 @@ $CI->load->view("action_buttons",$action_data);
 
     jQuery(document).ready(function()
     {
-        turn_off_triggers();        
+        turn_off_triggers();
 
     });
 </script>
